ef07204c6d43393412aac847a91337a44df2f394
minamorl <minamorl@minamorl.com>
2016-10-26 00:23:50 +0900

    Basic parser of markdown

---
+++ src/parser.ts
@@ -0,0 +1,77 @@
+import * as P from "parsimmon"
+
+const whitespace = P.regexp(/\s+/m)
+const asterisk = P.string("*")
+const sharp = P.string("#")
+const plainStr = P.regexp(/[^\*\r\n]+/)
+const linebreak = P.string("\r\n").or(P.string("\n")).or(P.string("\r"))
+
+const surroundWith = (tag: string) => {
+  return (s: string) => {
+    return `<${tag}>${s}</${tag}>`
+  }
+}
+const token = (p: P.Parser<any>) => {
+  return p.skip(P.regexp(/\s*/m))
+}
+const h1 = token(P.seq(
+    sharp,
+    whitespace,
+  ).then(plainStr)).map(surroundWith("h1"))
+const h2 = token(P.seq(
+    sharp.times(2),
+    whitespace,
+  ).then(plainStr)).map(surroundWith("h2"))
+const h3 = token(P.seq(
+    sharp.times(3),
+    whitespace,
+  ).then(plainStr)).map(surroundWith("h3"))
+const h4 = token(P.seq(
+    sharp.times(4),
+    whitespace,
+  ).then(plainStr)).map(surroundWith("h4"))
+const h5 = token(P.seq(
+    sharp.times(5),
+    whitespace,
+  ).then(plainStr)).map(surroundWith("h5"))
+const h6 = token(P.seq(
+    sharp.times(6),
+    whitespace,
+  ).then(plainStr)).map(surroundWith("h6"))
+
+const strongStart = P.string("**")
+const strongEnd = strongStart
+const strong = strongStart
+  .then(plainStr)
+  .map(surroundWith("strong"))
+  .skip(strongEnd)
+
+const inline = strong.or(plainStr)
+
+const paragraph = inline.atLeast(1).map(x => x.join("")).map(surroundWith("p"))
+
+const paragraphStart = P.regexp(/^(?!(#|```))/)
+const paragraphBreak =  linebreak.atLeast(2).result("")
+
+const paragraphOrLinebreak = paragraph
+  .or(paragraphBreak)
+  .atLeast(1)
+  .map(x => x.join(""))
+
+const acceptables = P.alt(
+    h6,
+    h5,
+    h4,
+    h3,
+    h2,
+    h1,
+    paragraphOrLinebreak,
+    whitespace.result("<br />"),
+  ).many().map(x => x.join(""))
+
+export const parse = (s: string) => {
+  const parsed = acceptables.parse(s)
+  if(parsed.hasOwnProperty("value"))
+    return parsed.value
+  throw new Error("Parsing was failed.")
+}
---
+++ test/test-parser.ts
@@ -0,0 +1,74 @@
+import * as assert from 'power-assert'
+
+import {parse} from "../src/parser"
+
+describe("parser", () => {
+  it('should parse h1', () => {
+    const input = "# header"
+    const expect = "<h1>header</h1>"
+    assert.equal(parse(input), expect)
+  })
+  it('should parse h2', () => {
+    const input = "## header"
+    const expect = "<h2>header</h2>"
+    assert.equal(parse(input), expect)
+  })
+  it('should parse h3', () => {
+    const input = "### header"
+    const expect = "<h3>header</h3>"
+    assert.equal(parse(input), expect)
+  })
+  it('should parse h4', () => {
+    const input = "#### header"
+    const expect = "<h4>header</h4>"
+    assert.equal(parse(input), expect)
+  })
+  it('should parse h5', () => {
+    const input = "##### header"
+    const expect = "<h5>header</h5>"
+    assert.equal(parse(input), expect)
+  })
+  it('should parse h6', () => {
+    const input = "###### header"
+    const expect = "<h6>header</h6>"
+    assert.equal(parse(input), expect)
+  })
+  it('should parse single line as a paragraph', () => {
+    const input = `para1`
+    const expect = "<p>para1</p>"
+    assert.equal(parse(input), expect)
+  })
+  it('should parse a paragraph', () => {
+    const input = `para1
+
+para2`
+    const expect = "<p>para1</p><p>para2</p>"
+    assert.equal(parse(input), expect)
+  })
+  it('should parse a paragraph', () => {
+    const input = `para1
+
+
+para2`
+    const expect = "<p>para1</p><p>para2</p>"
+    assert.equal(parse(input), expect)
+  })
+  it('should parse bold text inside a paragraph', () => {
+    const input = `this should be **bold**`
+    const expect = "<p>this should be <strong>bold</strong></p>"
+    assert.equal(parse(input), expect)
+  })
+
+  it('should parse bold text a single phrase', () => {
+    const input = `**bold**`
+    const expect = "<p><strong>bold</strong></p>"
+    assert.equal(parse(input), expect)
+  })
+  it('should parse complex text', () => {
+    const input = `
+# header1
+paragraph`
+    const expect = "<br /><h1>header1</h1><p>paragraph</p>"
+    assert.equal(parse(input), expect)
+  })
+})
