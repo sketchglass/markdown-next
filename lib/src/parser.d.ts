9268280c315b5aa1fda0552c220e38ff93028084
minamorl <minamorl@minamorl.com>
2016-11-29 11:50:58 +0900

    Split as class

--- lib/src/parser.d.ts
+++ lib/src/parser.d.ts
@@ -1,1 +1,21 @@
+/// <reference types="parsimmon" />
+import P = require("parsimmon");
+export interface ListTree {
+    type: "ul" | "ol" | "shadow";
+    children: Array<ListTree>;
+    value: string | null;
+    parent: ListTree | null;
+}
+export declare class Parser {
+    liLevelBefore: number | null;
+    liLevel: number | null;
+    rootTree: ListTree;
+    currentTree: ListTree;
+    acceptables: P.Parser<string>;
+    constructor(opts?: {
+        silent: boolean;
+        mapper: (tagName: string) => (children: any) => string;
+    });
+    parse(s: string): string | undefined;
+}
 export declare const parse: (s: string) => string | undefined;
